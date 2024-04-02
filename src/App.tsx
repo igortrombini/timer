8f7e89c409c67ca6673b582b16b2f7f1c458fc81
igortrombini <igorts20@gmail.com>
2024-04-01 22:32:39 -0300

    add: globalstyle

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,18 @@
+import { ThemeProvider } from 'styled-components';
 import { Button } from "./Components/Button";
+import { defaultTheme } from './styles/theme/default';
+import { GlobalStyle } from './styles/global';
 
 export function App() {
   return (
-    <>
+    <ThemeProvider theme={defaultTheme}>
       <Button variant="primary" />
       <Button variant="secundary" />
       <Button variant="danger" />
       <Button variant="success" />
       <Button />
 
-    </>
+      <GlobalStyle />
+    </ThemeProvider >
   )
 }
--- src/Components/Button.styles.ts
+++ src/Components/Button.styles.ts
@@ -17,7 +17,11 @@ export const ButtonContainer = styled.button<ButtonContainerProps>`
 width: 100px;
 height: 40px;
 
-${props => {
-        return css`background-color: ${buttonVariant[props.variant]}`
-    }}
+background-color: ${props => props.theme["green-500"]};
+color: ${props => props.theme.white};
+
+/* ${props => {
+        return css
+            `background-color: ${buttonVariant[props.variant]}`
+    }} */
 `
