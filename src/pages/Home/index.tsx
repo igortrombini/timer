3f856e2e3348e6106b2a16b2ed43306b8d9f83db
igortrombini <igorts20@gmail.com>
2024-04-10 00:23:43 -0300

    add:styles

---
+++ src/pages/Home/index.tsx
@@ -0,0 +1,31 @@
+import { Play } from "phosphor-react";
+import { CountDownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+
+export function Home() {
+    return (
+        <HomeContainer>
+            <form action="">
+                <FormContainer>
+                    <label htmlFor="task">Vou trabalhar em</label>
+                    <TaskInput id="task" placeholder="Dê um nome para seu projeto"/>
+
+                    <label htmlFor="minutesAmount">durante</label>
+                    <MinutesAmountInput type="number" id="minutesAmount" placeholder="00" />
+                    <span>minutos</span>
+                </FormContainer>
+
+                <CountDownContainer>
+                    <span>0</span>
+                    <span>0</span>
+                    <Separator>:</Separator>
+                    <span>0</span>
+                    <span>0</span>
+                </CountDownContainer>
+                <StartCountdownButton disabled type="submit">
+                    <Play size={24} />
+                    Começar
+                </StartCountdownButton>
+            </form>
+        </HomeContainer>
+    )
+}
